a31c9c1bc5e16e35a019874cc08c7753267d9fd4
lerenzo martinez <martinezlerenzo@gmail.com>
2022-12-23 15:57:39 -0600

    immplented permission to use location services on iphone.

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,16 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import MapView from 'react-native-maps'
+import * as Location from 'expo-location'
 
 export default function App() {
 
-    const [ mapRegion, setMapRegion] = useState ({
+  
+  
+  const [ mapRegion, setMapRegion] = useState ({
       latitude: 37.78825,
       longitude: -122.4324,
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
     })
+
+  const [location, setLocation] = useState(null)
+  const [errorMsg, setErrorMsg] = useState(null)
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
 
   return (
@@ -19,6 +46,7 @@ export default function App() {
         style={{ alignSelf: 'stretch', height: '100%' }}
         region={mapRegion}
       />
+      <Text style={styles.paragraph}>{text}</Text>
     </View>
 
   );
